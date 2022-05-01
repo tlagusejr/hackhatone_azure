e57a7a241865f4e6a03e86ab09b6adec66c3fd18
심현덕_T2125 <52907198+tlagusejr@users.noreply.github.com>
2022-05-02 07:32:20 +0900

    Update inference.py

--- inference.py
+++ inference.py
@@ -6,11 +6,11 @@ from msrest.authentication import ApiKeyCredentials
 import os, time, uuid
 
 # Replace with valid values
-ENDPOINT = "https://cropsclassificationinstance-prediction.cognitiveservices.azure.com/"
-training_key = "caf5658a51004555a0437ae5c204a826"
-prediction_key = "c12dc0a24d76499d81dad2b9e41f0d63"
-prediction_resource_id = "/subscriptions/ce4ffcb6-a4a0-4527-800c-e99012407d50/resourceGroups/test-resource/providers/Microsoft.CognitiveServices/accounts/cropsclassificationinstance-Prediction"
-publish_iteration_name = "classifyModel"
+ENDPOINT = 
+training_key = 
+prediction_key = 
+prediction_resource_id = 
+publish_iteration_name = 
 
 credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
 trainer = CustomVisionTrainingClient(ENDPOINT, credentials)
